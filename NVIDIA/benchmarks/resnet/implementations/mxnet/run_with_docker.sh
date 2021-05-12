5bd9e4fb02efb49ab24bd3821fd1521b8a2c008e
ShawnXuan <xiexuanx2@gmail.com>
2021-05-12 12:41:56 +0800

    recover params

--- NVIDIA/benchmarks/resnet/implementations/mxnet/run_with_docker.sh
+++ NVIDIA/benchmarks/resnet/implementations/mxnet/run_with_docker.sh
@@ -26,7 +26,7 @@ fi
 readonly _seed_override=${SEED:-}
 readonly _config_file="./config_${DGXSYSTEM}.sh"
 readonly _logfile_base="${LOGDIR}/${DATESTAMP}"
-readonly _cont_name=rn50
+readonly _cont_name=image_classification
 _cont_mounts=("--volume=${DATADIR}:/data" "--volume=/tmp/log:/results")
 
 # MLPerf vars
